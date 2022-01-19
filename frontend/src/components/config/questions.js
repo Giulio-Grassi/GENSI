abc6dd142abdce0cb16a2520327c40e3c959c0d1
Ninad Bhuiyan <k19036591@kcl.ac.uk>
2022-01-19 17:28:26 +0000

    Update questions based on point 1 provided by Imke

--- frontend/src/components/config/questions.js
+++ frontend/src/components/config/questions.js
@@ -2,40 +2,118 @@ export default function getQuestions() {
     return (
         (
             {
-                "surveyId": "SE88301",
-                "upperBoundNodes" : 2,
-                "lowerBoundNodes" : 3,
-                //means we must have at least 2 nodes and at maximum 3 nodes in the survey.
+                surveyId: "FIRST001-Imke",
+                upperBoundNodes: 10,
+                lowerBoundNodes: 10,
             },
             [
                 {
                     "id": 1,
-                    "text" : "do you like them",
-                    "type" : "ladder",
+                    "text" : "Who knows who?",
+                    "type" : "network",
+                    "filterYou": true,
+                },
+                {
+                    "id": 2,
+                    "text" : "Which person did you contact yourself, by taking the initiative?",
+                    "type" : "noderow",
+                    "filterYou": true,
+                },
+                {
+                    "id": 3,
+                    "text" : "Please indicate how often you have contact with these people.",
+                    "type" : "linebox",
                     "filterYou": true,
                     "boxes" : [
                         {
-                            "id": "Like a lot", 
+                            "id": "Daily", 
                             "colour": "#AED6F1"
                         },
                         {
-                            "id": "Like somehow",
+                            "id": "Several times a week",
                             "colour": "#E74C3C"
                         },
                         {
-                            "id": "Like a little less", 
+                            "id": "Once a week", 
                             "colour": "#C948F5"
                         },
                         {
-                            "id": "Don't like",
+                            "id": "Once a month",
                             "colour": "#E74C3C"
+                        },
+                        {
+                            "id": "Less often",
+                            "colour": "#AED6F1"
                         },
                     ]
                 },
                 {
-                    "id": 2,
-                    "text" : "who do you like",
-                    "type" : "noderow",
+                    "id": 4,
+                    "text" : "Please indicate how often you have contact with this person, by clicking on the name, and dragging/dropping that contact into the appropriate category.",
+                    "type" : "linebox",
+                    "filterYou": true,
+                    "boxes" : [
+                        {
+                            "id": "Partner", 
+                            "colour": "#AED6F1"
+                        },
+                        {
+                            "id": "Friend",
+                            "colour": "#E74C3C"
+                        },
+                        {
+                            "id": "Colleague", 
+                            "colour": "#C948F5"
+                        },
+                        {
+                            "id": "Health care",
+                            "colour": "#E74C3C"
+                        },
+                        {
+                            "id": "Professional",
+                            "colour": "#AED6F1"
+                        },
+                        {
+                            "id": "Family", 
+                            "colour": "#C948F5"
+                        },
+                        {
+                            "id": "Neighbour",
+                            "colour": "#E74C3C"
+                        },
+                        {
+                            "id": "Other",
+                            "colour": "#AED6F1"
+                        },
+                    ]
+                },
+                {
+                    "id": 5,
+                    "text" : "Imagine you are in the middle, please indicate the age of your connections, as a little older than you (1 above), or much older than you (2 above), a little younger than you (1 below), much younger (2 below), or the same (also in the middle).",
+                    "type" : "ladder",
+                    "filterYou": true,
+                    "boxes" : [
+                        {
+                            "id": "Much older", 
+                            "colour": "#AED6F1"
+                        },
+                        {
+                            "id": "A little older",
+                            "colour": "#E74C3C"
+                        },
+                        {
+                            "id": "Same age", 
+                            "colour": "#C948F5"
+                        },
+                        {
+                            "id": "A little younger",
+                            "colour": "#E74C3C"
+                        },
+                        {
+                            "id": "Much younger",
+                            "colour": "#AED6F1"
+                        },
+                    ]
                 },
             ]
         )
