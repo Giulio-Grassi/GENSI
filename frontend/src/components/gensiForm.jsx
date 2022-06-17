5358c578a0c44dbe4116f400661d7f01c22a3291
Giulio-Grassi <giulio.gg.grassi@gmail.com>
2022-06-17 13:55:39 +0100

    remnants

--- frontend/src/components/gensiForm.jsx
+++ frontend/src/components/gensiForm.jsx
@@ -17,8 +17,8 @@ import QuestionStrategy from './questionStrategy';
 import getQuestions from './config/questions'
 import './assets/css/styles.css'
 import axios from 'axios';
-import saveAnswersOnDatabase from './uploadAnswers'
-import saveAnswersSeparately from './uploadSingleAnswers'
+// import saveAnswersOnDatabase from './uploadAnswers'
+// import saveAnswersSeparately from './uploadSingleAnswers'
 
 export default function GensiForm(props) {
   const [step, setStep] = React.useState(1)
@@ -33,6 +33,182 @@ export default function GensiForm(props) {
   const [questions, setQuestion] = React.useState(questionArray); //React state containing the array of questions
   const [table, setTable] = React.useState(new Table()); //State containing the MxN relationship table
   
+
+
+
+  function saveAnswersOnDatabase(){
+      // e.preventDefault()
+      var counter = 1
+      var anonymizedNames = []
+      nodes.forEach(x => {
+        var t = {
+          name: x.getName(),
+          anonName: counter++,
+        }
+        anonymizedNames.push(t)
+      })
+  
+      const totalAnswers = []
+  
+      for(let i = 0; i < questions.length; i++){
+        var results = table.getAll().filter(x => x[0] === i+1)
+        var answer = {}
+  
+        if(results && results.length > 0){
+          if(results[0][3] === "mcq"){
+            /*1: { //mcq
+              questionType: "mcq"
+              title: "How do you like yourself?"
+              answer: "a lot",
+            },*/
+            answer = {
+              questionType: "mcq",
+              title: questions[i].getText(),
+              answer: results[0][2],
+            }
+          }
+          else if(results[0][3] === "ladder"){
+            /*2: { //ladder
+              questionType: "ladder"
+              title: "how do you like these people"
+              answer: [
+                1: {
+                  title: "like a lot"
+                  answer: [1,3]
+                }
+                2: {
+                  title: "like somehow"
+                  answer: [4]
+                }
+              ]
+            }*/
+            console.log("we inside ladder")
+            console.log(results)
+  
+            let boxesLadder  = questions[i].getBoxes()
+            var ans = []
+            boxesLadder.forEach(b => {
+              var ansArray = []
+              var boxResults = results.filter(x => x[2] === b.id)
+              boxResults.forEach(br => {
+                ansArray.push(anonymizedNames.filter(aN => aN.name === br[1])[0].anonName)
+              })
+  
+              var tempAns = {
+                title: b.id,
+                answer: ansArray
+              }
+  
+              ans.push(tempAns)
+            })
+  
+            answer = {
+              questionType: "ladder",
+              title: questions[i].getText(),
+              answer: ans
+            }
+          }
+          else if(results[0][3] === "linebox"){
+            /*2: {
+              questionType: "linebox"
+              title: "how do you like these people"
+              answer: [
+                1: {
+                  title: "like a lot"
+                  answer: [1,3]
+                }
+                2: {
+                  title: "like somehow"
+                  answer: [4]
+                }
+              ]
+            }*/
+            let boxesLadder  = questions[i].getBoxes()
+            var ans = []
+            boxesLadder.forEach(b => {
+              var ansArray = []
+              var boxResults = results.filter(x => x[2] === b.id)
+              boxResults.forEach(br => {
+                ansArray.push(anonymizedNames.filter(aN => aN.name === br[1])[0].anonName)
+              })
+  
+              var tempAns = {
+                title: b.id,
+                answer: ansArray
+              }
+  
+              ans.push(tempAns)
+            })
+  
+            answer = {
+              questionType: "linebox",
+              title: questions[i].getText(),
+              answer: ans
+            }
+          }
+          else if(results[0][3] === "noderow"){
+            /*{
+                  questionType: "noderow"
+                  title: "Has bullied someone",
+                  answer: {
+                      selected: [3],
+                      unselected: [1,4]
+              }
+            }*/
+            console.log("we inside noderow")
+            console.log(results)
+            var ansSelected = []
+            var andUnselected = []
+            results.forEach(r => {
+              if(r[2]){
+                ansSelected.push(anonymizedNames.filter(aN => aN.name === r[1])[0].anonName)
+              }else{
+                andUnselected.push(anonymizedNames.filter(aN => aN.name === r[1])[0].anonName)
+              }
+            })
+  
+            answer = {
+              questionType: "noderow",
+              title: questions[i].getText(),
+              answer: {
+                selected: ansSelected,
+                unselected: andUnselected
+              }
+            }
+          }
+          else if(results[0][3] === "network"){
+            //For network we save the object containing the relationships
+            answer = {
+              questionType: "network",
+              answer: results[0][1],
+            }
+          }
+          totalAnswers.push(answer)
+        }
+      }
+      const container = (surveyId, totalAnswers)
+      
+      //Post the event to mongodb
+      axios({
+        method: 'post',
+        url:'http://backend:8080/api/survey/add', 
+        data: {container}})
+          .then(
+              alert("Success.")
+          )
+          .catch((error) => {
+              alert("Something went wrong when saving your answers!")
+              console.log("Answers uploading error", error)
+          });    
+      console.log("Uploaded Answers",totalAnswers)  
+    }
+  
+
+
+
+
+
+
   // Proceed to next step
   function nextStep(){
     setStep(step + 1);
@@ -115,8 +291,9 @@ export default function GensiForm(props) {
               />
           );
           case 4:
-            saveAnswersOnDatabase(surveyId, questions, table, nodes)
-            saveAnswersSeparately(surveyId, questions, table, nodes)
+            // saveAnswersOnDatabase(surveyId, questions, table, nodes)
+            saveAnswersOnDatabase()
+            // saveAnswersSeparately(surveyId, questions, table, nodes)
             return (
               <Box id="paragraph page" fill= "vertical" justify="center" align="center" pad= "small" height="medium" >
                   <Text size="xxxlarge" className="title">
